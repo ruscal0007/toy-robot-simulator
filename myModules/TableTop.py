34844db00146a26f7809ccf4dabbd79c076e0d91
ruscal007 <ben.w@carbonetix.com.au>
2021-05-28 16:38:15 +1000

    Added functions to place objects and see if positions are free in the play area Still not enough code to run anything and havent started automated testing yet

--- myModules/TableTop.py
+++ myModules/TableTop.py
@@ -10,14 +10,30 @@ Created on Fri May 28 13:34:35 2021
 # Python doesnt inheirently support arrays so using multidimensional list to represent the play area of the tabletop
 # Should be able to store objects in row, column index of multidimension list so can accomodate for more than 1 robot in play area or other obstacles
 class TableTop:
-    def __init__(self, width = 5, length = 5):
+    def __init__(self, rows = 5, columns = 5):
         self.play_area = []
-        for j in range(width):
+        for j in range(rows):
             column = []
-            for i in range(length):
-                column.append(0)
+            for i in range(columns):
+                column.append(None)
             self.play_area.append(column)
 
     def showfield(self):
-        self.play_area[3][4] = 3
+
+#Print out the playfield in a nice format
+#for now just dumping it, will fix later
         print(self.play_area)
+        
+    def is_free(self,row,column):
+        if self.play_area[row][column] is not None:
+            return False
+        else:
+            return True
+
+#function to place object(could be robot or obstacle) on board for first time     
+    def place_object(self,theObj,row = 0,column = 0):
+#check if position is occupied
+        if self.is_free(row,column):
+            self.play_area[row][column] = theObj
+        else:
+            return False
