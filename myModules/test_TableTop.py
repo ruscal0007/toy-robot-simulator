754a5429fd3fe9f97ae5d23e5687df3fa13f0c2b
ruscal007 <ben.w@carbonetix.com.au>
2021-05-30 13:40:33 +1000

    Had to rebuild python virutal environment, pip broke for some reason and wouldnt install pytest module Started adding tests with pytest, will have to refactor some code, have rows and columns backwards in TableTop init

---
+++ myModules/test_TableTop.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 13:23:06 2021
+
+@author: ben
+"""
+
+# test_wallet.py
+
+import pytest
+from myModules.TableTop import *
+
+
+def test_default_play_area_size():
+    TT = TableTop()
+    assert len(TT.play_area[0]) == 5
+    assert len(TT.play_area[1]) == 5
+    
+def test_custom_pay_area_size():
+    TT = TableTop(20,50)
+    assert len(TT.play_area[0]) == 20
+    assert len(TT.play_area[1]) == 50
+    
